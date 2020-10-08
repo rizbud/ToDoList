494ba3829e0393e24d0b59c9519e753963ce5160
DeVoresyah ArEst <de.voresyah@gmail.com>
2020-10-08 13:59:11 +0700

    Add OsmiCSX

---
+++ app/themes/OsmiProvider.js
@@ -0,0 +1,11 @@
+import CustomTheme from './CustomTheme' // your custom style file
+import { OsmiProvider } from 'osmicsx'
+
+const provider = new OsmiProvider(customTheme);
+
+const { apply, connect } = provider;
+
+export {
+  apply,
+  connect
+}
